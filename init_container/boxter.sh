5e6280ba7e56d3c95819e062c836e09a68740322
lunz1207 <lunz1207@gmail.com>
2022-08-02 19:48:49 +0800

    feat: add demo

--- init_container/boxter.sh
+++ init_container/boxter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 ./mc config host add minio ${URL} ${USER} ${PASSWORD}
-./mc share download --json --expire 1h minio/${BUCKET_NAME}/${JOB_NAME} |jq '.url' | xargs wget -P /cache-jmx
+./mc share download --json --expire 1h minio/${BUCKET_NAME}/${TASK_NAME} |jq '.url' | xargs wget -P /cache-jmx
